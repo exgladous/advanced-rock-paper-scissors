c585a2c59a76f544b57d391a407ec03bb220d63b
Matthew J Gao <mg00805@srcs.loc>
2018-05-09 11:00:56 -0700

    reading files and readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Case Study
+------
+This is a repository containing the code for the case study assignment. Current on case study 1.
+
+trello link: https://trello.com/c/cFQsDpLF/2-game-mechanics
---
+++ rochambeau.js
@@ -0,0 +1,82 @@
+var playerChoice,
+    computerChoice,
+    rockButton = document.getElementById("rock"),
+    paperButton = document.getElementById("paper"),
+    scissorsButton = document.getElementById("scissors"),
+    playButton = document.getElementById("play"),
+    choices = ["Rock", "Paper", "Scissors"],
+    score = [0, 0, 0];
+
+rockButton.addEventListener('click', () => {
+    storePlayerChoice(0)
+});
+paperButton.addEventListener('click', () => {
+    storePlayerChoice(1)
+});
+scissorsButton.addEventListener('click', () => {
+    storePlayerChoice(2)
+});
+playButton.addEventListener('click', () => {
+    playGame()
+});
+
+function storePlayerChoice(choice) {
+    playerChoice = choice;
+    console.log("My choice = " + playerChoice);
+    storeComputerChoice();
+}
+
+function storeComputerChoice() {
+    computerChoice = Math.floor(Math.random() * 3);
+    console.log("Computer choice = " + computerChoice);
+}
+
+function playGame() {
+    if (playerChoice == computerChoice) {
+        console.log("tie");
+        updateScore(1);
+        displayGameResult("tie")
+    } else if (playerChoice == 0 && computerChoice == 2) {
+        console.log("win");
+        updateScore(0);
+        displayGameResult("win")
+    } else if (playerChoice == 1 && computerChoice == 0) {
+        console.log("win");
+        updateScore(0);
+        displayGameResult("win")
+    } else if (playerChoice == 2 && computerChoice == 1) {
+        console.log("win");
+        updateScore(0);
+        displayGameResult("win")
+    } else {
+        console.log("lose");
+        updateScore(2);
+        displayGameResult("lose")
+    }
+}
+
+function displayGameResult(result) {
+    var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
+    if (result === "win") {
+        document.getElementById("result").textContent = message + " YOU WIN!";
+        document.getElementById("result").className = "alert alert-success";
+    } else if (result === "lose") {
+        document.getElementById("result").textContent = message + " YOU LOOSE!";
+        document.getElementById("result").className = "alert alert-danger";
+    } else {
+        document.getElementById("result").textContent = message + " A tie.";
+        document.getElementById("result").className = "alert alert-info";
+    }
+    updateScoreBoard();
+}
+
+function updateScore(val) {
+    ++score[val];
+    console.log("The score is now " + score);
+}
+
+function updateScoreBoard() {
+    document.getElementById("wins").textContent = score[0];
+    document.getElementById("losses").textContent = score[2];
+    document.getElementById("ties").textContent = score[1];
+}
